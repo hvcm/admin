3b5f427715b4efaa58b3cd5811f49f2e9cefce2e
artificial-life <proteus.protocol@gmail.com>
2017-07-13 12:58:15 +0700

    upd

--- middleware/couchbase.js
+++ middleware/couchbase.js
@@ -1,7 +1,9 @@
 "use strict";
 const config = require('../config/default.js');
+const adminreports = require('./data.json');
 
 const couchbase = require('couchbase')
+const fs = require('fs')
 const cluster = new couchbase.Cluster(config.database.host);
 
 const cb = require("Couchbird");
@@ -11,9 +13,22 @@ const main = db.bucket(config.buckets.main);
 const auth = db.bucket(config.buckets.auth);
 const ViewQuery = couchbase.ViewQuery;
 
+const manager = main.manager();
+manager
+	.getDesignDocuments()
+	.then((res) => {
+		if (_.isEqual(res.adminreports, adminreports)) {
+			return;
+		}
+		manager
+			.upsertDesignDocument('adminreports', adminreports, function(err) {
+				console.log('Insertion of design document completed with error:', err);
+			});
+	});
+
 module.exports = function(req, res, next) {
 	req.cb = main;
 	req.auth = auth;
-	req.query = view => ViewQuery.from('reports', view);
+	req.query = view => ViewQuery.from('adminreports', view);
 	next();
 };
