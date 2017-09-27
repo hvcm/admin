a1fb523e837b2662548fc7f8c57b7de1656d640c
artificial-life <proteus.protocol@gmail.com>
2017-09-28 03:21:42 +0700

    upd

--- server/controllers/web-terminals.js
+++ server/controllers/web-terminals.js
@@ -8,13 +8,13 @@ class WebTerminals extends Basic {
 
 		return cb
 			.get('megatron-6')
-			.then(term => {
-				term.value.available_workstation = data;
-				return cb.upsert('megatron-6', term.value)
+			.then(({value}) => {
+				value.available_workstation = data;
+				console.log('WEB TERMINAL:', value);
+				return cb.upsert('megatron-6', value);
 			})
 			.then(res => this.res.json(res));
 	}
-
 }
 
 module.exports = WebTerminals;
